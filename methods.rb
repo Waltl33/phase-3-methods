f9d2ed447e9bc17a71fb6259c4b38203b2ffe4d3
Walter <walter.clark24@yahoo.com>
2023-01-22 09:29:45 -0500

    commit tree aed60e073c6490a7ed5cfc7f8292a661881b6ce8

--- methods.rb
+++ methods.rb
@@ -1,1 +1,17 @@
 # Your code here!
+def my_method(params)
+    puts "Running my_method"
+    param +1 
+end
+
+
+
+function sayHi(name) {
+  console.log(`Hi there, ${name}!`);
+}
+
+sayHi();
+
+def greet_programmer()
+    puts "Hello, programmer!";
+end
